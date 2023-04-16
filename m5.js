b59076d3e926a3d86e422a664b5f64bbcdcc9859
M9china <nqobilencaza2@gmail.com>
2023-04-16 16:55:54 +0200

    optimized code

--- m5.js
+++ m5.js
@@ -19,3 +19,35 @@ fetchData(url)
     console.log(data);
   })
   .catch((error) => console.log(error));
+
+  function createElement(type, attributes) {
+    const element = document.createElementNS("http://www.w3.org/2000/svg", type);
+      const text1 = document.createElementNS("http://www.w3.org/2000/svg",type);
+    for (let key in attributes) {
+      element.setAttribute(key, attributes[key]);
+    }
+    return element;
+  }
+  const svgholder = document.getElementById("imageX")
+  const svg1 = createElement('svg', { width: '100', height: '100' });
+  const circle1 = createElement('circle', {
+    cx: 50,
+    cy: 50,
+    r: 40,
+    style: 'fill: red; stroke: purple; stroke-width: 4px;'
+  });
+  const textholder = document.getElementById("txt")
+  const text1 = createElement('text', {
+    x: 50,
+    y: 50,
+    'text-anchor': 'middle',
+    'dominant-baseline': 'middle',
+    fill: 'white',
+    textContent: 'I am svg'
+  });
+  fetchData()
+  
+  svg1.appendChild(circle1);
+  textholder.appendChild(text1);
+  svgholder.appendChild(svg1);
+  
--- server/index.js
+++ server/index.js
@@ -2,11 +2,44 @@ import express from "express";
 import cors from "cors";
 const app = express();
 const port = 4377;
-app.use(express.json());
+app.use(express.json());// ?
 app.use(cors({ origin: "*" }));
 app.get("/", (req, resp) => {
-  resp.status(200).send({ message: "Voilor!", lessons: [4, 5, 5] });
+  resp.status(200).send({ message: "", lessons });
 });
 app.listen(port, () => {
   console.log("Server running on port http://localhost:4377");
 });
+
+var lessons = [
+  {
+    topic:'Module 1',
+    id:1,
+    description:'html',
+    outcomes:['','','']
+  },
+  {
+    topic:'Module 2',
+    id:2,
+    description:'html 5 and css 3',
+    outcomes:['','','']
+  },
+  {
+    topic:'Module 3',
+    id:3,
+    description:'javascript',
+    outcomes:['','','']
+  },
+  {
+    topic:'Module 4',
+    id:4,
+    description:'DOM Manipulation',
+    outcomes:['','','']
+  },
+  {
+    topic:'Module 5',
+    id:5,
+    description:'Communicating with remote server',
+    outcomes:['','','']
+  }
+]
